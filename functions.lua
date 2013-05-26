82160f63de05ac269d84f2981951655d567485a3
Stephen McDonald <steve@jupo.org>
2013-05-27 08:56:38 +1000

    Initial import.

---
+++ functions.lua
@@ -0,0 +1,32 @@
+
+function list_pop ()
+  local l = redis.call('LRANGE', KEYS[1], 0, -1)
+  local i = tonumber(ARGV[1]) + 1
+  local v = table.remove(l, i)
+  redis.call('DEL', KEYS[1])
+  redis.call('RPUSH', KEYS[1], unpack(l))
+  return v
+end
+
+function list_insert ()
+  local l = redis.call('LRANGE', KEYS[1], 0, -1)
+  local i = tonumber(ARGV[1]) + 1
+  table.insert(l, i, ARGV[2])
+  redis.call('DEL', KEYS[1])
+  redis.call('RPUSH', KEYS[1], unpack(l))
+end
+
+function list_reverse ()
+  local l = redis.call('LRANGE', KEYS[1], 0, -1)
+  redis.call('DEL', KEYS[1])
+  redis.call('LPUSH', KEYS[1], unpack(l))
+end
+
+function list_multiply ()
+  local l = redis.call('LRANGE', KEYS[1], 0, -1)
+  local i = tonumber(ARGV[1]) + 1
+  local v = table.remove(l, i)
+  redis.call('DEL', KEYS[1])
+  redis.call('RPUSH', KEYS[1], unpack(l))
+  return v
+end
---
+++ hot_redis.py
@@ -0,0 +1,123 @@
+
+from contextlib import contextmanager
+from uuid import uuid4
+from redis import Redis
+
+
+redis = Redis()
+_pipeline = None
+client = lambda: _pipeline or redis
+
+@contextmanager
+def pipeline():
+    global _pipeline
+    _pipeline = redis.pipeline()
+    yield
+    _pipeline.execute()
+    _pipeline = None
+
+lua_funcs = {}
+with open("functions.lua", "r") as f:
+    for func in f.read().strip().split("function "):
+        if not func:
+            continue
+        name, code = func.split("\n", 1)
+        name = name.split("(")[0].strip()
+        code = code.rsplit("end", 1)[0].strip()
+        lua_funcs[name] = redis.register_script(code)
+
+
+class Base(object):
+
+    def __init__(self, value=None, key=None):
+        self.key = key or str(uuid4())
+
+    def __getattr__(self, name):
+        try:
+            func = getattr(client(), name)
+        except AttributeError:
+            pass
+        else:
+            return lambda *args: func(self.key, *args)
+        try:
+            func = lua_funcs[name]
+        except KeyError:
+            pass
+        else:
+            return lambda *args: func(keys=[self.key], args=args)
+        raise AttributeError
+
+
+class List(Base):
+
+    def __init__(self, value=None, key=None):
+        super(List, self).__init__(value, key)
+        self.extend(value or [])
+
+    def __iter__(self):
+        return iter(self[:])
+
+    def __add__(self, l):
+        return List(self[:] + l)
+
+    def __iadd__(self, l):
+        self.extend(l)
+        return self
+
+    def __imul__(self, i):
+        self.extend(self[:] * (i - 1))
+        return self
+
+    def __mul__(self, i):
+        return List(self[:] * i)
+
+    def __contains__(self, value):
+        return value in self[:]
+
+    def __repr__(self):
+        return "%s" % self[:]
+
+    def __len__(self):
+        return self.llen()
+
+    def __getitem__(self, i):
+        if isinstance(i, slice):
+            if i.start is None:
+                i.start = 0
+            if i.stop is None:
+                i.stop = -1
+            return self.lrange(i.start, i.stop)
+        item = self.lindex(i)
+        if item is None:
+            raise IndexError
+        return item
+
+    def __setitem__(self, i, value):
+        self.lset(i, value)
+
+    def extend(self, l):
+        self.rpush(*l)
+
+    def append(self, value):
+        self.extend([value])
+
+    def insert(self, i, value):
+        self.list_insert(i, value)
+
+    def pop(self, i=-1):
+        if i == -1:
+            return self.rpop()
+        elif i == 0:
+            return self.lpop()
+        else:
+            return self.list_pop(i)
+
+    def reverse(self):
+        self.list_reverse()
+
+    def index(self, value):
+        self[:].index(value)
+
+    def count(self, value):
+        self[:].count(value)
+
