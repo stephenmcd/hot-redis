ac14f1a7a95e80862a0840539d9a318248ec61cb
Stephen McDonald <steve@jupo.org>
2013-05-27 12:21:17 +1000

    Implement Lua list_multiply

--- functions.lua
+++ functions.lua
@@ -24,9 +24,10 @@ end
 
 function list_multiply()
     local l = redis.call('LRANGE', KEYS[1], 0, -1)
-    local i = tonumber(ARGV[1]) + 1
-    local v = table.remove(l, i)
     redis.call('DEL', KEYS[1])
-    redis.call('RPUSH', KEYS[1], unpack(l))
-    return v
+    local i = tonumber(ARGV[1])
+    while i > 0 do
+        i = i - 1
+        redis.call('RPUSH', KEYS[1], unpack(l))
+    end
 end
