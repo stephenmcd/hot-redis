14e58c53bcdf5e6ceb69b5b00324fc4243e85334
Stephen McDonald <steve@jupo.org>
2014-05-26 15:37:18 +1000

    Move client code into its own module.

--- hot_redis/__init__.py
+++ hot_redis/__init__.py
@@ -5,5 +5,6 @@ except ImportError:
     pass
 else:
     from .types import *
+    from .client import *
 
 __version__ = "0.2.0"
--- hot_redis/types.py
+++ hot_redis/types.py
@@ -1,95 +1,13 @@
 
 import collections
-import contextlib
 import operator
-import os
 import Queue as _Queue
-import threading
 import time
 import uuid
 
 import redis
 
-
-class HotClient(redis.Redis):
-    """
-    A Redis client wrapper that loads Lua functions and creates
-    client methods for calling them.
-    """
-
-    def __init__(self, *args, **kwargs):
-        super(HotClient, self).__init__(*args, **kwargs)
-        requires_luabit = ("number_and", "number_or", "number_xor",
-                           "number_lshift", "number_rshift")
-        with open(self._get_lua_path("bit.lua")) as f:
-            luabit = f.read()
-        for name, snippet in self._get_lua_funcs():
-            if name in requires_luabit:
-                snippet = luabit + snippet
-            self._create_lua_method(name, snippet)
-
-    def _get_lua_path(self, name):
-        """
-        Joins the given name with the relative path of the module.
-        """
-        parts = (os.path.dirname(os.path.abspath(__file__)), "lua", name)
-        return os.path.join(*parts)
-
-    def _get_lua_funcs(self):
-        """
-        Returns the name / code snippet pair for each Lua function
-        in the atoms.lua file.
-        """
-        with open(self._get_lua_path("atoms.lua")) as f:
-            for func in f.read().strip().split("function "):
-                if func:
-                    bits = func.split("\n", 1)
-                    name = bits[0].split("(")[0].strip()
-                    snippet = bits[1].rsplit("end", 1)[0].strip()
-                    yield name, snippet
-
-    def _create_lua_method(self, name, code):
-        """
-        Registers the code snippet as a Lua script, and binds the
-        script to the client as a method that can be called with
-        the same signature as regular client methods, eg with a
-        single key arg.
-        """
-        script = self.register_script(code)
-        method = lambda key, *a, **k: script(keys=[key], args=a, **k)
-        setattr(self, name, method)
-
-
-
-_thread = threading.local()
-_config = {}
-
-
-def default_client():
-    try:
-        _thread.client
-    except AttributeError:
-        setattr(_thread, "client", HotClient(**_config))
-    return _thread.client
-
-
-def configure(config):
-    global _config
-    _config = config
-
-
-@contextlib.contextmanager
-def transaction():
-    """
-    Swaps out the current client with a pipeline instance,
-    so that each Redis method call inside the context will be
-    pipelined. Once the context is exited, we execute the pipeline.
-    """
-    client = default_client()
-    _thread.client = client.pipeline()
-    yield
-    _thread.client.execute()
-    _thread.client = client
+from .client import default_client
 
 
 ####################################################################
